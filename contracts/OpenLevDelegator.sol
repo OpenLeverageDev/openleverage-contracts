dd7345dab32e77cf6c0f710fc6aaba6635d70691
jimmy-openlev <hello@openleverage.finance>
2021-08-05 16:20:27 +0800

    Update licenses

--- contracts/OpenLevDelegator.sol
+++ contracts/OpenLevDelegator.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.7.3;
 pragma experimental ABIEncoderV2;
 
--- contracts/dex/PriceOracleV2.sol
+++ contracts/dex/PriceOracleV2.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.7.3;
 
 import "./PriceOracleInterface.sol";
