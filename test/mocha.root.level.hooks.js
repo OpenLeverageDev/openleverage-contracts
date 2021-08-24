d4e9c080eafd99810a48b39631bec1b9e8a40da3
Jimmy <hello@openleverage.finance>
2021-08-24 21:33:01 +0800

    Change to approx equal for vesting test

--- test/mocha.root.level.hooks.js
+++ test/mocha.root.level.hooks.js
@@ -1,13 +1,8 @@
-const argv = require('minimist')(process.argv.slice(2), {string: ['custom_argument']});
-var logger = require("mocha-logger");
+const logger = require("mocha-logger");
 
-var mochaTestCount = 0;
-var fastMode = false;
+let mochaTestCount = 0;
 
 beforeEach(function () {
     this.currentTest.id = ++mochaTestCount;
     logger.log("\n[TEST " + this.currentTest.id + "]: " + this.currentTest.title);
-    if (argv['mode'] == "fast")
-        fastMode = true;
-    console.log("Running mode: " + argv['mode']);
 });
