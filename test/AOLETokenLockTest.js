233207992469ae5dfd542f7856796a07b2dec6e4
ColaMin <94951512+ColaM12@users.noreply.github.com>
2022-02-18 12:01:18 +0800

    revert OLETokenLock

--- test/AOLETokenLockTest.js
+++ test/AOLETokenLockTest.js
@@ -10,60 +10,60 @@ const m = require('mocha-logger');
 const timeMachine = require('ganache-time-traveler');
 
 contract("OLETokenLock", async accounts => {
-    let oleToken;
+    before(async () => {
 
-    beforeEach(async () => {
-        oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
     });
 
     it("Take out all at one time, the time has expired: ", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1]], [toWei(100000)], ['1599372311'], [(parseInt(await lastBlockTime()))]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
-        assert.equal((await oleTokenLock.releaseAbleAmount(accounts[1])), toWei(100000).toString());
-        await oleTokenLock.releaseAll({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[1]});
         assert.equal((await oleTokenLock.releaseAbleAmount(accounts[1])), 0);
         // Comparison of results
         assert.equal(toBN(100000).mul(toBN(1e18)).toString(), (await oleToken.balanceOf(accounts[1])).toString());
+
     });
 
     it("The withdrawal address is non beneficiary: ", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1]], [toWei(100000)], ['1599372311'], [(parseInt(await lastBlockTime()))]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
-        await assertThrows(oleTokenLock.releaseAll({from: accounts[2]}), 'nothing to release');
+        await assertThrows(oleTokenLock.release({from: accounts[2]}), 'beneficiary does not exist');
     });
 
     it("Cash withdrawal before start time:", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1]], [toWei(100000)], [(parseInt(await lastBlockTime()) + 30000).toString().substr(0, 10)], [(parseInt(await lastBlockTime()) + 60000) + ""]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
-        await assertThrows(oleTokenLock.releaseAll({from: accounts[1]}), 'nothing to release');
+        await assertThrows(oleTokenLock.release({from: accounts[1]}), 'not time to unlock');
     });
 
     it("Withdraw twice, is the amount correct each time: ", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1]], [toWei(100000)], ['1599372311'], [(parseInt(await lastBlockTime()) + 30000)]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
-        await oleTokenLock.releaseAll({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[1]});
         assert.equal(firstStr("999", 3), firstStr((await oleToken.balanceOf(accounts[1])).toString(), 3));
 
         m.log("Wait for 10 seconds ....");
         let takeSnapshot = await timeMachine.takeSnapshot();
         let shotId = takeSnapshot['result'];
         await timeMachine.advanceTime(30000);
-        await oleTokenLock.releaseAll({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[1]});
 
         // Comparison of results
         assert.equal(firstStr("99999", 5), firstStr((await oleToken.balanceOf(accounts[1])).toString(), 5));
         await timeMachine.revertToSnapshot(shotId);
+
     });
 
     it("After one withdrawal, is the result correct at end time", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
+
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1]], [toWei(100000)], ['1599372311'], [(parseInt(await lastBlockTime()) + 30000)]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
-        await oleTokenLock.releaseAll({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[1]});
         // comparison of results
         assert.equal(firstStr("999", 3), firstStr((await oleToken.balanceOf(accounts[1])).toString(), 3));
 
@@ -71,7 +71,7 @@ contract("OLETokenLock", async accounts => {
         let takeSnapshot = await timeMachine.takeSnapshot();
         let shotId = takeSnapshot['result'];
         await timeMachine.advanceTime(30000);
-        await oleTokenLock.releaseAll({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[1]});
 
         //comparison of results
         assert.equal(firstStr("99999999", 8), firstStr((await oleToken.balanceOf(accounts[1])).toString(), 8));
@@ -79,70 +79,75 @@ contract("OLETokenLock", async accounts => {
     });
 
     it("If the withdrawal is completed, is it wrong to withdraw again: ", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
         // 2020-09-06 14:05:11  2021-01-01 14:05:11
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1]], [toWei(100000)], ['1599372311'], [(parseInt(await lastBlockTime()))]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
-        await oleTokenLock.releaseAll({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[1]});
         // comparison of results
         assert.equal(toWei(100000).toString(), (await oleToken.balanceOf(accounts[1])).toString());
-        await assertThrows(oleTokenLock.releaseAll({from: accounts[1]}), 'nothing to release');
+        await assertThrows(oleTokenLock.release({from: accounts[1]}), 'no amount available');
     });
 
-
     it("Two accounts, two addresses, partial withdrawal: ", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
         // 2020-09-06 14:05:11  2021-01-01 14:05:11
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1], accounts[3]], [toWei(100000), toWei(100000)],
             ['1599372311', '1599372311'], [(parseInt(await lastBlockTime()) + 0) + "", (parseInt(await lastBlockTime()) + 0) + ""]);
 
-        m.log("userHoldings account1:", await oleTokenLock.getUserHoldings(accounts[1]));
-        m.log("userHoldings account3:", await oleTokenLock.getUserHoldings(accounts[3]));
-
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
 
-        await oleTokenLock.releaseAll({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[1]});
 
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
-        await oleTokenLock.releaseAll({from: accounts[3]})
+        await oleTokenLock.release({from: accounts[3]})
 
         // comparison of results
         assert.equal(toWei(100000).toString(), (await oleToken.balanceOf(accounts[1])).toString());
 
         assert.equal(toWei(100000).toString(), (await oleToken.balanceOf(accounts[3])).toString());
-
     });
 
     it("transfer to A error with beneficiary does not exist test", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1]], [toWei(100000)],
             [parseInt(await lastBlockTime())], [parseInt(await lastBlockTime()) + 10000]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
         await timeMachine.advanceTime(5000);
-        await assertThrows(oleTokenLock.transferTo(accounts[2], 0, toWei(20000), {from: accounts[3]}), 'release ID not found');
+        await assertThrows(oleTokenLock.transferTo(accounts[2], toWei(20000), {from: accounts[3]}), 'beneficiary does not exist');
     });
 
     it("transfer to A  error with locked end test", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1]], [toWei(100000)],
             [parseInt(await lastBlockTime())], [parseInt(await lastBlockTime()) + 1000]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
         await timeMachine.advanceTime(1001);
-        await assertThrows(oleTokenLock.transferTo(accounts[2], 0, toWei(20000), {from: accounts[1]}), 'nothing to transfer');
+        await assertThrows(oleTokenLock.transferTo(accounts[2], toWei(20000), {from: accounts[1]}), 'locked end');
     });
-    
+
+    it("transfer to a exit account error with to is exist test", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
+        let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1],accounts[2]], [toWei(100000),toWei(100000)],
+            [parseInt(await lastBlockTime()),parseInt(await lastBlockTime())], [parseInt(await lastBlockTime()) + 1000,parseInt(await lastBlockTime()) + 1000]);
+        await oleToken.transfer(oleTokenLock.address, toWei(200000));
+        await timeMachine.advanceTime(10);
+        await assertThrows(oleTokenLock.transferTo(accounts[2], toWei(20000), {from: accounts[1]}), 'to is exist');
+    });
+
     it("transfer to A succeed test", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1]], [toWei(100000)],
             [parseInt(await lastBlockTime())], [parseInt(await lastBlockTime()) + 10000]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
         let shotId = (await timeMachine.takeSnapshot())['result'];
         await timeMachine.advanceTime(5000);
-        await oleTokenLock.releaseAll({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[1]});
         await timeMachine.advanceTime(1000);
-        await oleTokenLock.transferTo(accounts[2], 0, toWei(20000), {from: accounts[1]});
+        await oleTokenLock.transferTo(accounts[2], toWei(20000), {from: accounts[1]});
         await timeMachine.advanceTime(4000);
-        await oleTokenLock.releaseAll({from: accounts[1]});
-        await oleTokenLock.releaseAll({from: accounts[2]});
+        await oleTokenLock.release({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[2]});
         assert.equal((await oleTokenLock.releaseVars(accounts[1])).end,(await oleTokenLock.releaseVars(accounts[2])).end);
 
         // comparison of results
@@ -152,47 +157,30 @@ contract("OLETokenLock", async accounts => {
     });
 
     it("transfer to A,A transfer to B test", async () => {
+        let oleToken = await OLEToken.new(accounts[0], accounts[0], 'TEST', 'TEST');
         let oleTokenLock = await OLETokenLock.new(oleToken.address, [accounts[1]], [toWei(100000)],
             [parseInt(await lastBlockTime())], [parseInt(await lastBlockTime()) + 10000]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
         await oleToken.transfer(oleTokenLock.address, toWei(100000));
         let shotId = (await timeMachine.takeSnapshot())['result'];
         await timeMachine.advanceTime(5000);
-        await oleTokenLock.releaseAll({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[1]});
         await timeMachine.advanceTime(1000);
-        await oleTokenLock.transferTo(accounts[2], 0, toWei(20000), {from: accounts[1]});
+        await oleTokenLock.transferTo(accounts[2], toWei(20000), {from: accounts[1]});
         await timeMachine.advanceTime(2000);
-        await oleTokenLock.transferTo(accounts[3], 1, toWei(5000), {from: accounts[2]});
+        await oleTokenLock.transferTo(accounts[3], toWei(5000), {from: accounts[2]});
         await timeMachine.advanceTime(2000);
         assert.equal((await oleTokenLock.releaseVars(accounts[1])).end,(await oleTokenLock.releaseVars(accounts[2])).end);
         assert.equal((await oleTokenLock.releaseVars(accounts[2])).end,(await oleTokenLock.releaseVars(accounts[3])).end);
 
-        await oleTokenLock.releaseAll({from: accounts[1]});
-        await oleTokenLock.releaseAll({from: accounts[2]});
-        await oleTokenLock.releaseAll({from: accounts[3]});
+        await oleTokenLock.release({from: accounts[1]});
+        await oleTokenLock.release({from: accounts[2]});
+        await oleTokenLock.release({from: accounts[3]});
         // comparison of results
         assert.equal(toWei(80000).toString(), (await oleToken.balanceOf(accounts[1])).toString());
         assert.equal(toWei(15000).sub(await oleToken.balanceOf(accounts[2])).lt(toBN(100)), true);
         assert.equal(toWei(5000).sub(await oleToken.balanceOf(accounts[2])).lt(toBN(100)), true);
 
         await timeMachine.revertToSnapshot(shotId);
-
-    });
-
-    it("transfer to a exsited account success with 2 releases", async () => {
-        let oleTokenLock = await OLETokenLock.new(
-            oleToken.address, 
-            [accounts[1], accounts[2]], 
-            [toWei(100000), toWei(100000)],
-            [parseInt(await lastBlockTime()) + 100, parseInt(await lastBlockTime()) + 100], 
-            [parseInt(await lastBlockTime()) + 1100, parseInt(await lastBlockTime()) + 1100]);
-        m.log("userHoldings:", await oleTokenLock.getUserHoldings(accounts[1]));
-        await oleToken.transfer(oleTokenLock.address, toWei(200000));
-        await timeMachine.advanceTime(10);
-        await oleTokenLock.transferTo(accounts[2], 0, toWei(20000), {from: accounts[1]});
-
-        await assert.equal((await oleTokenLock.lockedAmount(accounts[1])).toString(), toWei(80000).toString());
-        await assert.equal((await oleTokenLock.lockedAmount(accounts[2])).toString(), toWei(120000).toString());
     });
 })
 
