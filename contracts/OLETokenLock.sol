b6c51992c3fc6ef2c29addff6e44d3664cd2317b
ColaMin <94951512+ColaM12@users.noreply.github.com>
2022-02-18 12:07:55 +0800

    revert OLETokenLock

--- contracts/OLETokenLock.sol
+++ contracts/OLETokenLock.sol
@@ -1,175 +1,87 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.7.6;
 
-pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./gov/OLEToken.sol";
-import "./lib/Utils.sol";
+
 
 /// @title OLE token Locked
 /// @author OpenLeverage
 /// @notice Release OLE to beneficiaries linearly.
 contract OLETokenLock {
-    using SafeMath for uint;
+    using SafeMath for uint256;
+    OLEToken public token;
+    mapping(address => ReleaseVar) public releaseVars;
 
-    OLEToken public oleToken;
-    mapping(uint => ReleaseVar) public releaseVars;
-    mapping(uint => address) public ownerOf;
-    mapping(address => uint[]) public holdings;
-    uint public releaseVarsCount;
-
-    event Mint(address indexed owner, uint indexed releaseID, uint amount, uint startTime, uint endTime);
-    event TransferOut(address indexed owner, uint indexed releaseID, uint amount);
-    event Release(address indexed beneficiary, uint indexed releaseID, uint amount);
+    event Release(address beneficiary, uint amount);
+    event TransferTo(address beneficiary, address to, uint amount);
 
     struct ReleaseVar {
-        uint amount;
+        uint256 amount;
         uint128 startTime;
         uint128 endTime;
         uint128 lastUpdateTime;
     }
 
-    constructor(OLEToken token, address[] memory beneficiaries, uint[] memory amounts, uint128[] memory startTimes, uint128[] memory endTimes) {
+    constructor(OLEToken token_, address[] memory beneficiaries, uint256[] memory amounts, uint128[] memory startTimes, uint128[] memory endTimes) {
         require(beneficiaries.length == amounts.length
         && beneficiaries.length == startTimes.length
             && beneficiaries.length == endTimes.length, "Array length must be same");
-        
-        oleToken = token;
-        for (uint i = releaseVarsCount; i < beneficiaries.length; i++) {
-            // require(startTimes[i] > block.timestamp, "too late to start");
-            require(endTimes[i] > startTimes[i], "startTime must earlier than endTime");
-            
+        token = token_;
+        for (uint i = 0; i < beneficiaries.length; i++) {
+            require(endTimes[i] > startTimes[i], "StartTime must be earlier than endTime");
             address beneficiary = beneficiaries[i];
-            ReleaseVar memory releaseVar = ReleaseVar(amounts[i], startTimes[i], endTimes[i], startTimes[i]);
-            releaseVars[i] = releaseVar;
-            ownerOf[i] = beneficiary;
-            holdings[beneficiary].push(i);
-
-            emit Mint(beneficiary, i, amounts[i], startTimes[i], endTimes[i]);
-        }
-
-        releaseVarsCount += beneficiaries.length;
-    }
-
-    function release(uint[] memory releaseIDs) external {
-        require(releaseInternal(releaseIDs) > 0, "nothing to release");
-    }
-
-    function releaseAll() external{
-        require(releaseInternal(holdings[msg.sender]) > 0, "nothing to release");
-    }
-
-    function releaseInternal(uint[] memory releaseIDs) internal returns (uint releaseAmount){
-        for (uint i; i < releaseIDs.length; i++){
-            uint releaseID = releaseIDs[i];
-            ReleaseVar memory releaseVar = releaseVars[releaseID];
-            if (block.timestamp > releaseVar.startTime && releaseVar.amount > 0){
-                releaseAmount += amountToRelease(releaseVar);
-                if (block.timestamp < releaseVar.endTime){
-                    releaseVars[releaseID].lastUpdateTime = uint128(block.timestamp);
-                }else{
-                    delete releaseVars[releaseID];
-                    delete ownerOf[releaseID];
-                }
-                emit Release(msg.sender, releaseID, releaseAmount);
-            }
-        }
-
-        if (releaseAmount > 0){
-            // The transfer out limit exceeds the available limit of the account
-            require(oleToken.balanceOf(address(this)) >= releaseAmount, "transfer out limit exceeds");
-            oleToken.transfer(msg.sender, releaseAmount);
+            releaseVars[beneficiary] = ReleaseVar(amounts[i], startTimes[i], endTimes[i], startTimes[i]);
         }
     }
 
-    function transferTo(address to, uint releaseID, uint amount) external {
-        require(ownerOf[releaseID] == msg.sender, "release ID not found");
-        require(to != msg.sender, 'same address');
-        // release first
-        {
-            uint[] memory releaseIDs = new uint[](1);
-            releaseIDs[0] = releaseID;
-            releaseInternal(releaseIDs);
-        }
+    function release() external {
+        releaseInternal(msg.sender);
+    }
 
+    function releaseInternal(address beneficiary) internal {
+        uint256 amount = token.balanceOf(address(this));
+        require(amount > 0, "no amount available");
+        uint256 releaseAmount = releaseAbleAmount(beneficiary);
+        // The transfer out limit exceeds the available limit of the account
+        require(amount >= releaseAmount, "transfer out limit exceeds ");
+        releaseVars[beneficiary].lastUpdateTime = uint128(block.timestamp > releaseVars[beneficiary].endTime ? releaseVars[beneficiary].endTime : block.timestamp);
+        token.transfer(beneficiary, releaseAmount);
+        emit Release(beneficiary, releaseAmount);
+    }
+
+    function transferTo(address to, uint amount) external {
+        address beneficiary = msg.sender;
+        require(releaseVars[beneficiary].amount > 0, 'beneficiary does not exist');
+        require(releaseVars[to].amount == 0, 'to is exist');
+        require(to != beneficiary, 'same address');
+        // release firstly
+        releaseInternal(beneficiary);
         // calc locked left amount
-        ReleaseVar memory releaseVar = releaseVars[releaseID];
-        require(releaseVar.amount > 0, "nothing to transfer");
-        uint lockedLeftAmount = amountLocked(releaseVar);
+        uint lockedLeftAmount = lockedAmount(beneficiary);
         require(lockedLeftAmount >= amount, 'Not enough');
-
-        uint128 startTime = uint128(Utils.maxOf(releaseVar.startTime, block.timestamp));
-        uint128 endTime = releaseVar.endTime;
-        releaseVars[releaseID].amount = lockedLeftAmount.sub(amount);
-        releaseVars[releaseID].startTime = startTime;
-
-        uint newReleaseID = releaseVarsCount;
-        releaseVarsCount++;
-        releaseVars[newReleaseID] = ReleaseVar(amount, startTime, endTime, startTime);
-        ownerOf[newReleaseID] = to;
-        holdings[to].push(newReleaseID);
-
-        emit TransferOut(msg.sender, releaseID, amount);
-        emit Mint(to, newReleaseID, amount, startTime, endTime);
+        releaseVars[beneficiary].amount = lockedLeftAmount.sub(amount);
+        uint128 startTime = uint128(releaseVars[beneficiary].startTime > block.timestamp ? releaseVars[beneficiary].startTime : block.timestamp);
+        releaseVars[beneficiary].startTime = startTime;
+        releaseVars[to] = ReleaseVar(amount, startTime, releaseVars[beneficiary].endTime, startTime);
+        emit TransferTo(beneficiary, to, amount);
     }
 
-    function amountToRelease(ReleaseVar memory releaseVar) internal view returns (uint) {
-        uint calTime = Utils.minOf(block.timestamp, releaseVar.endTime);
+    function releaseAbleAmount(address beneficiary) public view returns (uint256){
+        ReleaseVar memory releaseVar = releaseVars[beneficiary];
+        require(block.timestamp >= releaseVar.startTime, "not time to unlock");
+        require(releaseVar.amount > 0, "beneficiary does not exist");
+        uint256 calTime = block.timestamp > releaseVar.endTime ? releaseVar.endTime : block.timestamp;
         return calTime.sub(releaseVar.lastUpdateTime).mul(releaseVar.amount)
         .div(releaseVar.endTime - releaseVar.startTime);
     }
 
-    function amountLocked(ReleaseVar memory releaseVar) internal pure returns (uint){
-        return releaseVar.amount.mul(releaseVar.endTime - releaseVar.lastUpdateTime).div(releaseVar.endTime - releaseVar.startTime);
+    function lockedAmount(address beneficiary) public view returns (uint256){
+        ReleaseVar memory releaseVar = releaseVars[beneficiary];
+        require(releaseVar.endTime >= block.timestamp, 'locked end');
+        return releaseVar.amount.mul(releaseVar.endTime - releaseVar.lastUpdateTime)
+        .div(releaseVar.endTime - releaseVar.startTime);
     }
 
-    function releaseAbleAmount(address beneficiary) external view returns (uint amount){
-        uint[] memory releaseIDs = holdings[beneficiary];
-        for (uint i; i < releaseIDs.length; i++){
-            ReleaseVar memory releaseVar = releaseVars[releaseIDs[i]];
-            if(block.timestamp >= releaseVar.startTime && releaseVar.amount > 0){
-                amount += amountToRelease(releaseVar);
-            }
-        }
-    }
-
-    function lockedAmount(address beneficiary) external view returns (uint amount){
-        uint[] memory releaseIDs = holdings[beneficiary];
-        for (uint i; i < releaseIDs.length; i++){
-            ReleaseVar memory releaseVar = releaseVars[releaseIDs[i]];
-            if(releaseVar.endTime >= block.timestamp){
-                amount += amountLocked(releaseVar);
-            }
-        }
-    }
-
-    function getUserHoldings(address beneficiary) external view returns (uint[] memory validHoldings){
-        uint[] memory userHoldings = holdings[beneficiary];
-        uint count;
-        for (uint i; i < userHoldings.length; i++){
-            if (releaseVars[userHoldings[i]].amount > 0){
-                count ++;
-            }
-        }
-        validHoldings = new uint[](count);
-
-        count = 0;
-        for (uint i; i < userHoldings.length; i++){
-            if (releaseVars[userHoldings[i]].amount > 0){
-                validHoldings[count] = userHoldings[i];
-                count ++;
-            }
-        }
-    }
-
-    function reArrangeUserHoldings(address beneficiary) external{
-        uint[] memory userHoldings = holdings[beneficiary];
-        delete holdings[beneficiary];
-        for (uint i; i < userHoldings.length; i++){
-            if (releaseVars[userHoldings[i]].amount > 0){
-                holdings[beneficiary].push(userHoldings[i]);
-            }
-        }
-    }
 }
