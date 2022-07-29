f2b8605d323759805e5c8e0da1ea9f86dabe362b
Fatel <david@openleverage.finance>
2022-07-29 12:02:35 +0800

    support limit order

--- contracts/OpenLevInterface.sol
+++ contracts/OpenLevInterface.sol
@@ -59,6 +59,8 @@ abstract contract OpenLevStorage {
     // map(marketId, tokenAddress, index) => taxRate)
     mapping(uint16 => mapping(address => mapping(uint => uint24))) public taxes;
 
+    address public opLimitOrder;
+
     event MarginTrade(
         address trader,
         uint16 marketId,
@@ -137,7 +139,11 @@ interface OpenLevInterface {
 
     function marginTrade(uint16 marketId, bool longToken, bool depositToken, uint deposit, uint borrow, uint minBuyAmount, bytes memory dexData) external payable;
 
+    function marginTradeFor(address trader, uint16 marketId, bool longToken, bool depositToken, uint deposit, uint borrow, uint minBuyAmount, bytes memory dexData) external payable returns (uint256);
+
     function closeTrade(uint16 marketId, bool longToken, uint closeAmount, uint minOrMaxAmount, bytes memory dexData) external;
+
+    function closeTradeFor(address trader, uint16 marketId, bool longToken, uint closeHeld, uint minOrMaxAmount, bytes memory dexData) external returns (uint256);
 
     function liquidate(address owner, uint16 marketId, bool longToken, uint minBuy, uint maxAmount, bytes memory dexData) external;
 
@@ -145,11 +151,7 @@ interface OpenLevInterface {
 
     function updatePrice(uint16 marketId, bytes memory dexData) external;
 
-    function shouldUpdatePrice(uint16 marketId, bytes memory dexData) external view returns (bool);
-
     function getMarketSupportDexs(uint16 marketId) external view returns (uint32[] memory);
-
-    // function getCalculateConfig() external view returns (OpenLevStorage.CalculateConfig memory);
 
     /*** Admin Functions ***/
     function setCalculateConfig(uint16 defaultFeesRate, uint8 insuranceRatio, uint16 defaultMarginLimit, uint16 priceDiffientRatio,
@@ -164,5 +166,7 @@ interface OpenLevInterface {
     function setSupportDex(uint8 dex, bool support) external;
 
     function setTaxRate(uint16 marketId, address token, uint index, uint24 tax) external;
+
+    function setOpLimitOrder(address _opLimitOrder) external;
 
 }
