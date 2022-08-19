2bf4795375c2178eb6c98f81147e63db5cf8d944
Paul-D-Ant <anthony@openleverage.finance>
2022-08-19 15:49:07 +0800

    update verify-script.sh

--- verify-script.sh
+++ verify-script.sh
@@ -3,20 +3,22 @@
 echo starting verify contact network $1.
 
 truffle run verify ControllerV1 --network  $1
-truffle run verify EthDexAggregatorV1 --network $1
-truffle run verify LPool --network  $1
+truffle run verify CronosDexAggregatorV1 --network $1
+truffle run verify CronosUniV2Dex --network $1
+truffle run verify LTimePool --network  $1
 truffle run verify LPoolDepositor --network $1
+truffle run verify LPoolDepositorDelegator --network $1
 truffle run verify OpenLevV1 --network  $1
+truffle run verify OpenLevV1Lib --network  $1
+
 truffle run verify QueryHelper --network $1
 truffle run verify Timelock --network $1
 truffle run verify XOLE --network $1
 truffle run verify ControllerDelegator --network  $1
 truffle run verify DexAggregatorDelegator --network $1
-truffle run verify LPoolDelegator --network $1
-truffle run verify LPoolDepositor --network $1
 truffle run verify OpenLevDelegator --network $1
 truffle run verify XOLEDelegator --network $1
-
+truffle run verify Airdrop --network $1
 
 
 truffle run verify OLEToken --network  $1
@@ -28,3 +30,5 @@ truffle run verify OLETokenLock --network  $1
 truffle run verify FarmingPool --network  $1
 
 echo finished verify contact network $1.
+
+
