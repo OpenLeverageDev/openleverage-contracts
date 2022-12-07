cc834d9685c5c3d7b4bc9004776dd03d9e2b5a54
Paul-D-Ant <anthony@openleverage.finance>
2022-12-07 10:09:31 +0800

    access 1inch

--- contracts/Types.sol
+++ contracts/Types.sol
@@ -105,4 +105,32 @@ library Types {
         uint8 decimals;
         uint lastUpdateTime;
     }
+
+    struct FlashBuyVar {
+        address buyToken;
+        address sellToken;
+        uint buyAmount;
+        uint maxSellAmount;
+        uint closeAmount;
+        bytes data;
+        bytes marketDefaultDex;
+    }
+
+    struct FlashSellVar {
+        address buyToken;
+        address sellToken;
+        uint sellAmount;
+        uint minBuyAmount;
+        bytes data;
+    }
+
+    struct FeesAndInsuranceVar {
+        address trader;
+        uint tradeSize;
+        address token;
+        uint16 marketId;
+        uint totalHeld;
+        uint reserve;
+    }
+
 }
