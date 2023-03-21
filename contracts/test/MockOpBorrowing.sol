225ed25ec1932ba319992f23ffa9795c63ff3a22
Fatel <david@openleverage.finance>
2023-03-21 20:46:21 +0800

    fix borrowing test

--- contracts/test/MockOpBorrowing.sol
+++ contracts/test/MockOpBorrowing.sol
@@ -8,7 +8,7 @@ contract MockOpBorrowing {
     uint16 public  marketId;
     address public pool0;
     address public pool1;
-    mapping(address => mapping(uint16 => mapping(bool => uint))) public activeBorrows;
+    mapping(address => mapping(uint16 => mapping(bool => uint))) public activeCollaterals;
 
     function addMarket(uint16 _marketId, address _pool0, address _pool1, bytes memory dexData) external {
         marketId = _marketId;
@@ -17,7 +17,7 @@ contract MockOpBorrowing {
     }
 
     function setActiveBorrows(address borrower, uint16 marketId, bool collateralIndex, uint collateral) external {
-        activeBorrows[borrower][marketId][collateralIndex] = collateral;
+        activeCollaterals[borrower][marketId][collateralIndex] = collateral;
     }
 
 
